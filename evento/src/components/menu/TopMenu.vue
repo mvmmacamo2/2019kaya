715958ebd9a3c2dfc1f8413f5082c7a25d09952b
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2019-01-22 14:19:08 +0200

    ToolBar and Navigation Drawer

--- evento/src/components/menu/TopMenu.vue
+++ evento/src/components/menu/TopMenu.vue
@@ -1,4 +1,69 @@
 <template>
+  <v-app>
+    <v-navigation-drawer
+      stateless
+      value="true"
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile>
+
+        <v-list-group
+          prepend-icon="account_circle"
+          value="true"
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Users</v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-group
+            no-action
+            sub-group
+            value="true"
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-title>Admin</v-list-tile-title>
+            </v-list-tile>
+
+            <v-list-tile
+              v-for="(admin, i) in admins"
+              :key="i"
+              @click=""
+            >
+              <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+              <v-list-tile-action>
+                <v-icon v-text="admin[1]"></v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+
+          <v-list-group
+            sub-group
+            no-action
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-title>Actions</v-list-tile-title>
+            </v-list-tile>
+
+            <v-list-tile
+              v-for="(crud, i) in cruds"
+              :key="i"
+              @click=""
+            >
+              <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+              <v-list-tile-action>
+                <v-icon v-text="crud[1]"></v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar app class="pink darken-3" dark>
       <v-toolbar-title class="headline text-uppercase">
         <span>K</span>
@@ -18,6 +83,7 @@
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+  </v-app>
 </template>
 
 <script>
